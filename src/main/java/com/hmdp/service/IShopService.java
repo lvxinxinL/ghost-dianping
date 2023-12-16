1c0e12ff303e71ff0d81a268d79ef8d6c61ebe1a
Ghost <2089049079@qq.com>
2023-12-16 11:01:00 +0800

    add 添加商铺缓存 update 商铺缓存更新

--- src/main/java/com/hmdp/service/IShopService.java
+++ src/main/java/com/hmdp/service/IShopService.java
@@ -1,16 +1,29 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
- * <p>
- *  服务类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
+ * 商铺相关业务逻辑
+ * @author Ghost
+ * @version 1.0
  */
 public interface IShopService extends IService<Shop> {
 
+
+    /**
+     * 根据 id 查询商铺信息
+     * @param id 商铺id
+     * @return 商铺详情数据
+     */
+    Result queryById(Long id);
+
+
+    /**
+     * 更新商铺信息
+     * @param shop
+     * @return
+     */
+    Result update(Shop shop);
 }
--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,20 +1,82 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
 
 /**
- * <p>
- *  服务实现类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
+ * 商铺相关业务逻辑
+ * @author Ghost
+ * @version 1.0
  */
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 根据 id 查询商铺信息
+     * @param id 商铺id
+     * @return 商铺详情数据
+     */
+    public Result queryById(Long id) {
+        String key = CACHE_SHOP_KEY + id;
+        // 1. 查询 Redis
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+
+        // 2. 存在直接返回
+        if (StrUtil.isNotBlank(shopJson)) {
+            // 反序列化成 Java 对象
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+        
+        // 3. 不存在查询数据库
+        Shop shop = getById(id);
+
+        // 4. 数据库不存在返回错误信息
+        if(shop == null) {
+            return Result.fail("商铺不存在！");
+        }
+
+        // 5. 存在，写入 Redis，设置过期时间
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);// 序列化为字符串存入 Redis
+
+        // 6. 返回商铺查询结果
+        return Result.ok(shop);
+    }
+
+    /**
+     * 更新商铺信息
+     * @param shop
+     * @return
+     */
+    @Transactional
+    public Result update(Shop shop) {
+        Long id = shop.getId();
+        if(id == null) {
+            return Result.fail("商铺 id 不能为空");
+        }
+        // 1. 更新数据库
+        updateById(shop);
+
+        // 2. 删除缓存
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + id);
+        return Result.ok();
+    }
 }
