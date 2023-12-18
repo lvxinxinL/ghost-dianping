c04483cad7b980662f6f1be9380890a4f102583d
Ghost <2089049079@qq.com>
2023-12-18 20:09:47 +0800

    add 使用 Redis 生成全局唯一 id

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -3,11 +3,15 @@ package com.hmdp;
 import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisIDWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
@@ -19,7 +23,32 @@ class HmDianPingApplicationTests {
     private CacheClient cacheClient;
     @Resource
     private ShopServiceImpl shopService;
+    @Resource
+    private RedisIDWorker redisIDWorker;
 
+    // 准备线程池
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+
+    @Test
+    void testRedisIDWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIDWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("耗时：" + (end - begin));
+    }
     @Test
     void testSaveShop() {
         Shop shop = shopService.getById(1);
